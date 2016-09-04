670c464841bbfa1d59a10fa45243b1c632f53ce8
danikoren <dani_koren@yahoo.com>
2016-09-04 16:40:45 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -36,21 +36,24 @@ app.use((req, res) => {
       ReactDOM.renderToString(<Html assets={webpackIsomorphicTools.assets()} store={store} />))
   }
 
+  // Disable Server Side rendering flag
   if (__DISABLE_SSR__) {
     hydrateOnClient()
     return
   }
-
+  
+  // from react-router API
+  // 
   match({ history, routes: getRoutes(), location: req.url }, (error, redirectLocation, renderProps) => {
     if (error) {
       res.status(500).send(error.message)
     } else if (redirectLocation) {
       res.redirect(302, redirectLocation.pathname + redirectLocation.search)
     } else if (renderProps) {
-      loadOnServer({ ...renderProps, store }).then(() => {
+      loadOnServer({ ...renderProps, store }).then(() => { // from middleware redux-connect - delay the execution of our component
 
         const globalState = store.getState()
-
+        
         const component = (
           <Provider store={store} key="provider">
             <ReduxAsyncConnect {...renderProps} />
